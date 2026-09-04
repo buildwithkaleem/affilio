d24c912fab451653c2f779fb8f2c49af065a37c9
ranakaleem786 <ranakaleem786422@gmail.com>
2026-09-04 10:11:14 +0500

    set contects & notificaton

--- app/admin/notifications/page.jsx
+++ app/admin/notifications/page.jsx
@@ -223,7 +223,8 @@ export default function AdminNotificationsPage() {
 
               <Link
                 key={notification._id}
-                href={`/admin/notifications/${notification._id}`}
+                href={notification.type === "contact" ?
+                  `/admin/contact/${notification.referenceId}` : `/admin/notifications/${notification._id}`}
                 onClick={() =>
                   handleNotificationClick(
                     notification
